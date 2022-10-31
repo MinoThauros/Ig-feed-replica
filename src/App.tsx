7c8c985e445d31552df6b97e983a2e36584ddf29
MinoThauros <herimino@timatech.ca>
2022-10-31 10:16:04 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Typography} from "@mui/material"
+import { Add, Settings } from "@mui/icons-material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Button variant="text" color="secondary">Text</Button>
+    <Button variant="contained" startIcon={<Settings/>} color="secondary">SETTINGS</Button>
+    <Button variant="contained" startIcon={<Add/>} color="success">ADD NEW POST</Button>
+    
+
+    <Typography variant="h1" component="p">Playing with typography</Typography>
+    <Button variant="contained" sx={{
+      backgroundColor:'skyblue',
+      color:'#888',
+      margin:5,
+      "&:hover":{
+        backgroundColor:"lightblue"//catching dynamic states of UI
+      }
+      
+    }}>My unique Button</Button>
+  </div>
+
+
+    
   );
 }
 
 export default App;
+ 
